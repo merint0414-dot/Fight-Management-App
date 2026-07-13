e8aaafd0077154a484fc91d3dae64c8ede17084a
Joshua J <jj4440024@gmail.com>
2026-07-13 13:23:04 +0530

    Save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import NavBar from "./components/NavBar";
-import AddFlight from "./components/AddFlight";
-import ViewFlights from "./components/ViewFlights";
+import ViewFlights from './components/ViewFlights'
 
 function App() {
   return (
     <>
-      <NavBar />
-
-      <Routes>
-        <Route path="/" element={<AddFlight />} />
-        <Route path="/add-flight" element={<AddFlight />} />
-        <Route path="/flights" element={<ViewFlights />} />
-      </Routes>
+      <ViewFlights/>
     </>
   );
 }

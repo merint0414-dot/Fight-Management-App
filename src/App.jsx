d6c613196ad03104a959289a6234fc15022d2fd3
merint0414-dot <merint0414@gmail.com>
2026-07-13 12:56:45 +0530

    Complete view flight submission workflow

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import ViewFlights from './components/ViewFlights'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <ViewFlights/>
     </>
   )
 }

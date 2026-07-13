3b58226c05b26dc69b0eb7afa231d8b87aab202c
Joshua J <jj4440024@gmail.com>
2026-07-13 12:48:13 +0530

    Create Add Flight component

---
+++ src/components/AddFlight.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const AddFlight = () => {
+  return (
+    <div className="container mt-5">
+      <div className="card shadow">
+
+        <div className="card-header bg-primary text-white">
+          <h3 className="text-center">Add Flight</h3>
+        </div>
+
+        <div className="card-body">
+
+          <form>
+
+          </form>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default AddFlight;
